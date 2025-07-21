6491b32b5302bb87e8ccc1ac79dee2ab9b494a2f
tomduffeyjr <thomasrduffeyjr@gmail.com>
2025-07-21 14:33:39 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import pygame
+from constants import *
+
+
+
+
+
+def main():
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init() # Initializes pygame
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # This creates a game window
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill("black") # fills the screen with the color black
+
+        pygame.display.flip() # Refreshes the display
+
+if __name__ == "__main__":
+    main()
