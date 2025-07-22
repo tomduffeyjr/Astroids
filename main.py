24d09279b52c46730dcdd3e9cc2d8dba22f31254
tomduffeyjr <thomasrduffeyjr@gmail.com>
2025-07-22 00:39:50 -0500

    finished game

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import sys
 import pygame
 from constants import *
-
-
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
 
 
@@ -10,16 +13,52 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init() # Initializes pygame
+    clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable, drawable)
+    asteroid_field = AsteroidField()
+
+
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # This creates a game window
 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2) # initiates the player before the game loop and only once
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collision(player):
+                print("Game over!")
+                sys.exit()
+
+        for shot in shots:
+            for asteroid in asteroids:
+                if asteroid.collision(shot):
+                    shot.kill()
+                    asteroid.split()
+
+        
         screen.fill("black") # fills the screen with the color black
+
+        for obj in drawable:
+            obj.draw(screen)
 
         pygame.display.flip() # Refreshes the display
 
+
+        dt = clock.tick(60) / 1000 # limits frames to 60
 if __name__ == "__main__":
     main()
